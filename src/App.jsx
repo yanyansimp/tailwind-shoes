a2d243a642bc1c2ce99d8cbafbacd9e5bc9c9655
Rheyan Keneth B. Pahuyo <rpahuyo@gmail.com>
2024-10-04 17:15:37 +0800

    Shoe detail layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Nav } from "./components/Nav";
+import { ShoeDetail } from "./components/ShoeDetail";
 
 export function App() {
   return (
     <div className="p-10 xl:p-16">
-      <Nav />
+      <Nav/>
+      <ShoeDetail/>
     </div>
   );
 }
