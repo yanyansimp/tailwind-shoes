963ed806abc2983c149ef6df0300bf18b2ed7738
Rheyan Keneth B. Pahuyo <rpahuyo@gmail.com>
2024-10-04 15:15:39 +0800

    Mobile Nav Responsive mobile menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Nav } from "./components/Nav";
 
 export function App() {
   return (
-    <div className="p-10 xl:p-24">
+    <div className="p-10 xl:p-16">
       <Nav />
     </div>
   );
