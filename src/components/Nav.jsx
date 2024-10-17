68c9371e41586efda5ae15bac17c429fce88d588
Rheyan Keneth B. Pahuyo <rpahuyo@gmail.com>
2024-10-17 12:32:00 +0800

    Card layout

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import NikeLogo from "../assets/nike-logo.svg?react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { useState } from "react";
 
-const ROUTES = ["Home","About","Services","Pricing","Contact"];
+const ROUTES = ["Home", "About", "Services", "Pricing", "Contact"];
 
 export function Nav() {
   const [isMobileMenuShown, setIsMobileMenuShown] = useState(false);
@@ -37,7 +37,7 @@ export function Nav() {
                   i === 0
                     ? "bg-blue-500 text-white lg:bg-transparent lg:text-blue-500 "
                     : "hover:bg-gray-100"
-                } ${ (i === 3 || i === 4) && "lg:text-white" }`}
+                } ${(i === 3 || i === 4) && "lg:text-white"}`}
                 key={route}
               >
                 <a>{route}</a>
--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,22 +1,24 @@
 import { IoIosArrowDown } from "react-icons/io";
 
-export function Select({title, options}) {
+export function Select({ title, options }) {
   return (
     <div className="relative">
-        <select defaultValue={""} className="bg-white w-24 appearance-none border border-gray-300 p-4">
-            <option value="" disabled hidden>
-              {title}
-            </option>
-            {options.map(option => (
-              <option value={option} key={option}>
-                {option}
-              </option>
-            ))}
-        </select>
-        <div className="absolute inset-y-0 right-0 flex-center pr-3">
-          <IoIosArrowDown />
-        </div>
+      <select
+        defaultValue={""}
+        className="bg-white w-24 appearance-none border border-gray-300 p-4"
+      >
+        <option value="" disabled hidden>
+          {title}
+        </option>
+        {options.map((option) => (
+          <option value={option} key={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+      <div className="pointer-events-none absolute inset-y-0 right-0 flex-center pr-3">
+        <IoIosArrowDown />
+      </div>
     </div>
   );
 }
-
